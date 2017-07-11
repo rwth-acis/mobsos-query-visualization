165566b967ba9a13ff8b2474bc690e0784aa5451
AlexanderNeumann <alexander.t.n@web.de>
2017-07-11 12:08:55 +0200

    update start script

--- bin/start_network.bat
+++ bin/start_network.bat
@@ -5,5 +5,5 @@ cd ..
 set BASE=%CD%
 set CLASSPATH="%BASE%/lib/*;"
 
-java -cp %CLASSPATH% i5.las2peer.tools.L2pNodeLauncher -w -p 9011 uploadStartupDirectory('etc/startup') startService('i5.las2peer.services.servicePackage.ServiceClass','SampleServicePass') startWebConnector interactive
+java -cp %CLASSPATH% i5.las2peer.tools.L2pNodeLauncher -p 9011 "startService('i5.las2peer.services.mobsos.queryVisualization.QueryVisualizationService@0.6.7','someNewPass')" startWebConnector interactive
 pause
